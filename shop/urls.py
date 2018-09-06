17fc513644681923dcebf13c6930df8e8ef33184
DianaPatriche <dianapatriche.21@gmail.com>
2018-09-06 19:52:30 +0300

    am adaugat url-ul cu /id

--- shop/urls.py
+++ shop/urls.py
@@ -6,8 +6,9 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
+    url(r'^(?P<productid>\d+)/$', views.product_detail, name='product_detail'),
     re_path('^$', views.product_list, name='product_list'),
-    url(r'produs/', views.product_detail, name='product_details'),  #ce vine pe produs/ , mapeaza-l cu product_detail
+    #url(r'produs/', views.product_detail, name='product_details'),  #ce vine pe produs/ , mapeaza-l cu product_detail
     #url(r'^(?P<categoryid>\d+)/$', views.product_list, name='product_list_by_category'),
     #url(r'^(?P<productid>\d+)/$', views.product_detail, name='product_detail'),
 ]
