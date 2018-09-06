1b464526cb8612085d31a2f48841e9526f140ad6
DianaPatriche <dianapatriche.21@gmail.com>
2018-09-06 19:26:32 +0300

    adaugam categoriile si produsele

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,8 @@ from .models import Category, Product
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
-    context = {'products': products }
+    categorii = Category.objects.all()
+    context = {'products': products,'categorii': categorii }
     return render(request, 'list.html', context)
 
 
@@ -14,3 +15,4 @@ def product_detail(request):
     print(produs.id)
     context = {'produs': produs}
     return render(request, 'produs.html', context)
+
