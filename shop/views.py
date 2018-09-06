ec5778cbfa178c9b5b6739c7eab1818f18047b7f
DianaPatriche <dianapatriche.21@gmail.com>
2018-09-06 18:55:07 +0300

    first shopping cart commit

---
+++ shop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Category, Product
+
+# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products }
+    return render(request, 'list.html', context)
+
+
+def product_detail(request):
+    id = request.GET.get('id')
+    produs = Product.objects.get(pk=int(id))
+    print(produs.id)
+    context = {'produs': produs}
+    return render(request, 'produs.html', context)
